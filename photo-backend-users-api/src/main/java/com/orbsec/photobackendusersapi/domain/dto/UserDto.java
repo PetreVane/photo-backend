814ed71b3eae7f80ad1d24115d4fefa0180f6cea
Petre Vane <>
2021-08-25 17:30:24 +0200

    Added validation constraint

--- photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/domain/dto/UserDto.java
+++ photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/domain/dto/UserDto.java
@@ -2,24 +2,24 @@ package com.orbsec.photobackendusersapi.domain.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 public class UserDto {
 
-
+    @NotBlank(message = "First name cannot be empty")
     @Size(min = 2, max = 15, message = "First name must be at least 2 characters and maximum 15")
     private String firstName;
 
+    @NotBlank(message = "Last name cannot be empty")
     @Size(min = 2, max = 15, message = "Last name must be at least 2 characters and maximum 15")
     private String lastName;
 
-    @NotEmpty
+    @NotBlank(message = "Email cannot be empty")
     @Email(message = "Invalid email address")
     private String email;
 
-    @Size(min = 5, message = "Password must be at least 5 characters")
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 5, max = 15, message = "Password must be at least 5 characters and maximum 15 characters")
     private String password;
 }
