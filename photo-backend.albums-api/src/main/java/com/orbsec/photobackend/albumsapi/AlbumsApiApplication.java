c6d990ea2e60453612fb152bcc08ba515855b0ca
Petre Vane <>
2021-09-07 14:27:34 +0200

    Added new microservice

---
+++ photo-backend.albums-api/src/main/java/com/orbsec/photobackend/albumsapi/AlbumsApiApplication.java
@@ -0,0 +1,15 @@
+package com.orbsec.photobackend.albumsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AlbumsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApiApplication.class, args);
+	}
+
+}
---
+++ photo-backend.albums-api/src/test/java/com/orbsec/photobackend/albumsapi/AlbumsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbsec.photobackend.albumsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
