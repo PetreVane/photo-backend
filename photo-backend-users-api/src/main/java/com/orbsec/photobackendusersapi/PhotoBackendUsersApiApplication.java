e5855cbb1174bd1b3e836d64c7f4deaa068863e9
Petre Vane <>
2021-09-07 12:24:22 +0200

    Added asymmetric encryption of credentials

--- photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/PhotoBackendUsersApiApplication.java
+++ photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/PhotoBackendUsersApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -19,4 +20,5 @@ public class PhotoBackendUsersApiApplication {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 }
