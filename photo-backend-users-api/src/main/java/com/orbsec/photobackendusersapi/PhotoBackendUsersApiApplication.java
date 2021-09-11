65e38cddb1f35fcbd42ac34786ca6c5f34ff2004
Petre Vane <>
2021-09-11 15:50:12 +0200

    Added new dependency

--- photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/PhotoBackendUsersApiApplication.java
+++ photo-backend-users-api/src/main/java/com/orbsec/photobackendusersapi/PhotoBackendUsersApiApplication.java
@@ -2,11 +2,11 @@ package com.orbsec.photobackendusersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -21,4 +21,9 @@ public class PhotoBackendUsersApiApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate () {
+		return new RestTemplate();
+	}
 }
